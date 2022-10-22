3d426c0fae3427ae610c911e1a75aa0b6f7a8a66
khalid <khalid.a.montes@gmail.com>
2022-10-22 00:12:53 -0400

    commit tree 889ecf0b291e97a9ec752ea89fab1ce0c5f8c862

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+berforeEach(function () {
+    cats.length = 0;
+    
+    cats.push("Milo", "Otis", "Garfield");
+});
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    return cats;
+}
+function destructivelyRemoveLastCat() {
+    cats.pop(2);
+    return cats; 
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+    return cats;
+}
+function appendCat() {
+    const cats = ["Milo", "Otis", "Garfield"] 
+    const copyOfCats = [...cats, "Broom"]
+    return copyOfCats;
+}
+function prependCat() {
+    const cats = ["Milo", "Otis", "Garfield"]
+    const copyOfCats = ["Arnold", ...cats]
+    return copyOfCats;
+}
+function removeLastCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const copyOfCats = cats.slice(0, cats.length -1);
+    return copyOfCats;
+}
+function removeFirstCat() {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
